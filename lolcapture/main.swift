93ff5be39126634c6d22f0c58e5b3f29bbcd2367
Matthew Rothenberg <mrothenberg@gmail.com>
2015-02-19 01:18:26 -0500

    allow filename to be specified by user

--- lolcapture/main.swift
+++ lolcapture/main.swift
@@ -7,21 +7,25 @@ var programIdentifier: String {
     return "\(programName) \(programVersion)"
 }
 
-
-// TODO: #2 override from CLI
-// - parse CLI args
-// - check for parent directory existence
-// - open question: if pdir doesnt exist, do we mkdir -p? (probably not very unix-like)
-
 /// the path where the final image will be stored (including filename)
 let cwd = NSFileManager.defaultManager().currentDirectoryPath
-let fileName = "test-capture.jpg"
-let filePath = "\(cwd)/\(fileName)"
+let defaultFileName = "test-capture.jpg"
+
+/// the current filePath where we will write the completed image
+var filePath: String = cwd.stringByAppendingPathComponent(defaultFileName) {
+    didSet {
+        let writeOk = NSFileManager.defaultManager().isWritableFileAtPath(filePath)
+        if !writeOk {
+            println("Uhm, dude, I can't write to a file at \(filePath)")
+            exit(1)
+        }
+    }
+}
 
 /// default delay to pass on for warmup in capture process
 var delay = 0.75
 
-
+// CLI mode state tracking
 var testMode  = false
 var debugMode = false
 
@@ -51,11 +55,13 @@ func debug(group: String, msg: String) {
     }
 }
 
-/// Process arguments for the CLI
-func processArgs(args: [String]) {
-    for arg in args {
+/// Process dashed options for the CLI
+///
+/// :param: opts all options parsed from the command line
+func processDashedOpts(opts: [String]) {
+    for opt in opts {
 
-        let splitArg = arg.componentsSeparatedByString("=")
+        let splitArg = opt.componentsSeparatedByString("=")
         var argkey: String  = splitArg[0]
         var argval: String? = splitArg.count > 1 ? splitArg[1] : nil
         //debug("m", "key: \(key)")
@@ -66,7 +72,7 @@ func processArgs(args: [String]) {
         case "-h", "--help":
             // wow, no multiline string literals in Swift? Really?!
             let sep = "\t\t\t"
-            println("Usage: \(programName) [options]")
+            println("Usage: \(programName) [options] <destination>")
             println("")
             println("Experimental one-step webcam capture and text composition for lolcommits.")
             println("")
@@ -110,7 +116,7 @@ func processArgs(args: [String]) {
             debugMode = true
 
         default:
-            println("Unknown argument: \(arg)")
+            println("Unknown argument: \(opt)")
         }
     }
 }
@@ -147,6 +153,24 @@ func runCapture() {
 
 let arguments = NSProcessInfo.processInfo().arguments as [String]
 let appName = arguments[0].lastPathComponent
-let dashedArguments = arguments.filter({$0.hasPrefix("-")})
-processArgs(dashedArguments)
+let dashedOptions = arguments.filter({$0.hasPrefix("-")})
+let realArgs = arguments.filter({!$0.hasPrefix("-")})
+
+// we are assuming the second non-dashed option argument is the filename
+let potentialFileName: String? = (realArgs.count > 1) ? realArgs[1] : nil
+if let parsedFileName = potentialFileName {
+    
+    //FIXME: move me somewhere relatively sane please!
+    
+    // standardize the path to remove all junk
+    let standardPath = NSString(string: parsedFileName).stringByStandardizingPath
+
+    // is not an absolute path, prepend the current working directory
+    let absPath = NSString(string: parsedFileName).absolutePath
+    var resultingPath = absPath ? standardPath : cwd.stringByAppendingPathComponent(standardPath)
+
+    filePath = resultingPath
+}
+
+processDashedOpts(dashedOptions)
 runCapture()
