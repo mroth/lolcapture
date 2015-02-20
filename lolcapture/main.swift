20f337c4abe09f41caa4a845ffb6850e731dd1f1
Matthew Rothenberg <mrothenberg@gmail.com>
2015-02-19 19:18:41 -0500

    move configuration variables into struct
    no longer just global variables from main.swift

--- lolcapture/main.swift
+++ lolcapture/main.swift
@@ -7,40 +7,10 @@ var programIdentifier: String {
     return "\(programName) \(programVersion)"
 }
 
-/// the path where the final image will be stored (including filename)
-let cwd = NSFileManager.defaultManager().currentDirectoryPath
-let defaultFileName = "test-capture.jpg"
-
-/// the current filePath where we will write the completed image
-var filePath: String = cwd.stringByAppendingPathComponent(defaultFileName)
-
-/// default delay to pass on for warmup in capture process
-var delay = 0.75
-
-// CLI mode state tracking
-var testMode  = false
-var debugMode = false
-
-// msg/SHA to be used in test mode when nothing is parsed
-let testMessage = "this is a test message i didnt really commit something"
-var testSha: String {
-    return NSUUID().UUIDString.componentsSeparatedByString("-")[0].lowercaseString
-}
-
-// msg/SHA parsed from the CLI arguments
-var parsedMessage, parsedSha: String?
-
-// calculated message/SHA values to use
-var finalMessage: String? {
-    return (testMode && parsedMessage == nil) ? testMessage : parsedMessage
-}
-var finalSha: String? {
-    return (testMode && parsedSha == nil) ? testSha : parsedSha
-}
-
 /// Utility function to logs a debug message to STDOUT if DEBUG_MODE=true.
+// TODO: extract me into Logger class
 func debug(group: String, msg: String) {
-    if debugMode {
+    if Config.debugMode {
         let stderr = NSFileHandle.fileHandleWithStandardError()
         let debugStr = "DEBUG[\(group)]:\t\(msg)\n"
         stderr.writeData( debugStr.dataUsingEncoding(NSUTF8StringEncoding)! )
@@ -77,7 +47,7 @@ func processDashedOpts(opts: [String]) {
             println("  --test"        + sep + "Create fake msg/SHA values when none provided")
             println("  --msg=<MSG>"   + sep + "Message to be displayed across bottom of image")
             println("  --sha=<SHA>"   + sep + "Hash to be displayed on top right of image")
-            println("  --delay=<N>"   + sep + "Delay capture by N seconds (default: \(delay))")
+            println("  --delay=<N>"   + sep + "Delay capture by N seconds (default: \(Config.delay))")
             println("  --debug"       + sep + "Enable DEBUG output")
             exit(0)
 
@@ -91,23 +61,23 @@ func processDashedOpts(opts: [String]) {
             exit(0)
 
         case "-t", "--test":
-            testMode = true
+            Config.testMode = true
 
         case "--msg":
-            parsedMessage = argval
+            Config.parsedMessage = argval
 
         case "--sha":
-            parsedSha = argval
+            Config.parsedSha = argval
             
         case "--delay":
             if let delayStr = argval {
                 if let delayNum = NSNumberFormatter().numberFromString(delayStr) {
-                    delay = delayNum.doubleValue
+                    Config.delay = delayNum.doubleValue
                 }
             }
 
         case "--debug":
-            debugMode = true
+            Config.debugMode = true
 
         default:
             println("Unknown argument: \(opt)")
@@ -135,7 +105,7 @@ func parseArgs() -> (dashedOpts: [String], destinationFilePath: String?) {
         
         // if not an absolute path, prepend the current working directory
         let absPath = NSString(string: parsedFileName).absolutePath
-        parsedFilePath = absPath ? standardPath : cwd.stringByAppendingPathComponent(standardPath)
+        parsedFilePath = absPath ? standardPath : Config.cwd.stringByAppendingPathComponent(standardPath)
     }
     
     return (dashedOptions, parsedFilePath)
@@ -145,22 +115,22 @@ func parseArgs() -> (dashedOpts: [String], destinationFilePath: String?) {
 func runCapture() {
     println("📷 lolcommits is preserving this moment in history.")
 
-    if let rawimagedata = CamSnapper.capture(warmupDelay: delay) {
+    if let rawimagedata = CamSnapper.capture(warmupDelay: Config.delay) {
         if let lolimage = LOLImage(data: rawimagedata) {
-            lolimage.topMessage = finalSha
-            lolimage.bottomMessage = finalMessage
+            lolimage.topMessage = Config.finalSha
+            lolimage.bottomMessage = Config.finalMessage
 
-            let writeSuccess = lolimage.render().writeToFile(filePath, atomically: true)
+            let writeSuccess = lolimage.render().writeToFile(Config.filePath, atomically: true)
             if !writeSuccess {
-                println("ERROR: failure writing to file: \(filePath)")
+                println("ERROR: failure writing to file: \(Config.filePath)")
                 exit(1)
             }
             
-            debug("main", "LOL! image written to \(filePath)")
+            debug("main", "LOL! image written to \(Config.filePath)")
             
-            if testMode {
+            if Config.testMode {
                 // in test mode, open the image for preview immediately
-                NSWorkspace.sharedWorkspace().openFile(filePath)
+                NSWorkspace.sharedWorkspace().openFile(Config.filePath)
             }
         } else {
             println("ERROR: Didn't understand the image data we got back from camera.")
@@ -174,7 +144,7 @@ func main () {
     // parse the CLI arguments, override destination file path if provided by user
     let (dashedOptions, destinationFilePath) = parseArgs()
     if let dfp = destinationFilePath {
-        filePath = dfp
+        Config.filePath = dfp
     }
     
     // process dashed options to set up global state
