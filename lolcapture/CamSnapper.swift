642d864d7a4268c79921262417d050c49a7f2375
Matthew Rothenberg <mrothenberg@gmail.com>
2015-09-30 11:12:24 -0400

    another guard bouncer pattern

--- lolcapture/CamSnapper.swift
+++ lolcapture/CamSnapper.swift
@@ -43,82 +43,75 @@ class CamSnapper {
     /// - parameter warmupDelay: How long to delay capture for warmup (default: 0.0).
     /// - returns: The captured image data serialized to JPEG encoding.
     class func capture(warmupDelay: NSTimeInterval = 0.0, camera: AVCaptureDevice) -> NSData? {
-        let captureSession = AVCaptureSession()
-
-        // AVCaptureDevicInput is a failable initializer
-        // ...so in theory this should catch failure?
-        // ...in which case error proc isn't really needed, just Obj-C legacy
-        if let cameraInput = try? AVCaptureDeviceInput(device: camera) {
-
-            // begin configuration block
-            captureSession.beginConfiguration()
-
-            // set session defaults to photo capture
-            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
-
-            // set default input to the camera
-            captureSession.addInput(cameraInput)
-
-            // make a still image output object and make it the output target
-            let imageOutput = AVCaptureStillImageOutput()
-            captureSession.addOutput(imageOutput)
-
-            // commit configuration details and start session running
-            captureSession.commitConfiguration()
-            captureSession.startRunning()
-
-            // use dispatch group to know when (async) image capture is done.
-            //
-            // it's a shame there doesn't seem to be a built-in synchronous way
-            // to do this, since this is a rare occassion where we WANT to block
-            // the main thread.
-            let captureGroup = dispatch_group_create()
-            dispatch_group_enter(captureGroup)
-            Logger.debug("starting capture task")
-
-            // sleep on this thread for delay to enable full camera warm-up.
-            //
-            // AVFoundation seems to be good at waiting till the camera sees an
-            // image, but doesn't wait for white-balance & exposure adjustment.
-            Logger.debug("sleeping for \(warmupDelay) sec. to allow camera to warmup...")
-            NSThread.sleepForTimeInterval(warmupDelay)
-            Logger.debug("...warmup complete.")
-
-            // make a local optional var to hold the buffer after async capture
-            var imageBuffer: CMSampleBuffer? = nil
-
-            // do the async capture
-            let videoChannel = imageOutput.connectionWithMediaType(AVMediaTypeVideo)
-            imageOutput.captureStillImageAsynchronouslyFromConnection(
-                videoChannel,
-                completionHandler:{(buffer: CMSampleBuffer!, err: NSError!) in
-                    Logger.debug("finishing capture task")
-                    imageBuffer = buffer
-                    dispatch_group_leave(captureGroup)
-                }
-            )
-
-            // wait for dispatch group complete then stop session
-            let timeoutSec = warmupDelay + 10
-            let timeout = dispatch_time(DISPATCH_TIME_NOW, Int64(timeoutSec * Double(NSEC_PER_SEC)))
-            let dispatchResults = dispatch_group_wait(captureGroup, timeout)
-            captureSession.stopRunning()
-
-            // dispatch actually had timed out if results are non-zero
-            if dispatchResults != 0 {
-                print("ERROR! Timed out waiting for camera data.")
-                return nil
-            }
-
-            // all good, return some jpeg pics plz
-            return AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageBuffer)
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            Logger.debug("failed to initialize capture device!")
+            return nil
         }
 
-        // we shouldn't ever get here unless we can't get camera input.
+        // begin configuration session
+        let captureSession = AVCaptureSession()
+        captureSession.beginConfiguration()
+
+        // set session defaults to photo capture
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+
+        // set default input to the camera
+        captureSession.addInput(cameraInput)
+
+        // make a still image output object and make it the output target
+        let imageOutput = AVCaptureStillImageOutput()
+        captureSession.addOutput(imageOutput)
+
+        // commit configuration details and start session running
+        captureSession.commitConfiguration()
+        captureSession.startRunning()
+
+        // use dispatch group to know when (async) image capture is done.
         //
-        // its unfortunate the results of the failure condition way down here,
-        // but that seems to be a side-effect of using "if let" idomatic Swift.
-        return nil
+        // it's a shame there doesn't seem to be a built-in synchronous way
+        // to do this, since this is a rare occassion where we WANT to block
+        // the main thread.
+        let captureGroup = dispatch_group_create()
+        dispatch_group_enter(captureGroup)
+        Logger.debug("starting capture task")
+
+        // sleep on this thread for delay to enable full camera warm-up.
+        //
+        // AVFoundation seems to be good at waiting till the camera sees an
+        // image, but doesn't wait for white-balance & exposure adjustment.
+        Logger.debug("sleeping for \(warmupDelay) sec. to allow camera to warmup...")
+        NSThread.sleepForTimeInterval(warmupDelay)
+        Logger.debug("...warmup complete.")
+
+        // make a local optional var to hold the buffer after async capture
+        var imageBuffer: CMSampleBuffer? = nil
+
+        // do the async capture
+        let videoChannel = imageOutput.connectionWithMediaType(AVMediaTypeVideo)
+        imageOutput.captureStillImageAsynchronouslyFromConnection(
+            videoChannel,
+            completionHandler:{(buffer: CMSampleBuffer!, err: NSError!) in
+                Logger.debug("finishing capture task")
+                imageBuffer = buffer
+                dispatch_group_leave(captureGroup)
+            }
+        )
+
+        // wait for dispatch group complete then stop session
+        let timeoutSec = warmupDelay + 10
+        let timeout = dispatch_time(DISPATCH_TIME_NOW, Int64(timeoutSec * Double(NSEC_PER_SEC)))
+        let dispatchResults = dispatch_group_wait(captureGroup, timeout)
+        captureSession.stopRunning()
+
+        // dispatch actually had timed out if results are non-zero
+        if dispatchResults != 0 {
+            print("ERROR! Timed out waiting for camera data.")
+            return nil
+        }
+
+        // all good, return some jpeg pics plz
+        return AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageBuffer)
+
     }
 
 }
