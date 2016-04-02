6437717491a220b76f391d7b03031b9826a9f6c4
Matthew Rothenberg <mrothenberg@gmail.com>
2016-04-02 18:30:31 -0400

    use swift3 compat file/line markers

--- lolcapture/Logger.swift
+++ lolcapture/Logger.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct Logger {
     /// Log a debug message to STDERR if DEBUG_MODE=true.
-    static func debug(msg: String, file: String = __FILE__, line: Int = __LINE__, method: String = __FUNCTION__) {
+    static func debug(msg: String, file: String = #file, line: Int = #line, method: String = #function) {
         if DEBUG_MODE {
             let stderr = NSFileHandle.fileHandleWithStandardError()
 
